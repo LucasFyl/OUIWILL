5b941a09689e91a1e9089b0ef6271434bd73f836
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-20 19:16:30 +0200

    header setup + panel & permissions

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,7 +5,7 @@
     <li>
       <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
 
-      <?php if($p->hasVisibleChildren()): ?>
+      <!--<?php if($p->hasVisibleChildren()): ?>
       <ul class="submenu">
         <?php foreach($p->children()->visible() as $p): ?>
         <li>
@@ -13,7 +13,7 @@
         </li>
         <?php endforeach ?>
       </ul>
-      <?php endif ?>
+      <?php endif ?>-->
 
     </li>
     <?php endforeach ?>
