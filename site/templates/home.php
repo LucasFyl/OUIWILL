00ad885e457a2fbb5144b6231ad8bbb1a30c59a0
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-21 13:49:44 +0200

    understanding the grid and home html

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,13 +1,102 @@
 <?php snippet('head') ?>
-<?php snippet('header') ?>
 
-  <main class="main" role="main">
+	<section class="landing">
+		<header>
+			<a href="<?php $site->url() ?>" class="logo">oui will</a>
+			<p>precisely what you imagined your brand could be, only more so. ™</p>
+		</header>
+		
+		<div class="vertical-center">
+			<h1>Oui Will</h1>third biggest world watch events <br/>raising money to fight muscular dystrophy partnered <br/>with oui will for their new branding and website  
+		</div>
+		<a href="#" class="arrow-down"><i class="fa fa-arrow-down"></i></a>
+	</section>
 
-	<h1>Hello world</h1>
-	<h2>coucou</h2>
-	<h3>foo</h3>
-	<p>let's see about sizes</p>
 
-  </main>
+	<main class="main" id="home">
+		<section>
+			<div>
+				<h2>oui will is a full-service, integrated creative agency.</h2>
+				<p>we combine the european esthetic and the american yes we can. we create experiences in a digital world, helping people in their decisions for a brand, products and services.</p>
+			</div>
+		</section>
+
+		<section>
+			<!-- showreel -->
+		</section>
+
+		<section class="clients">
+			<h3>clients.</h3>
+
+			<div class="row">
+				<div class="two"><img src="#" alt="Audi"></div>
+				<div class="two"><img src="#" alt="Beaming"></div>
+				<div class="two"><img src="#" alt="Nixon"></div>
+				<div class="two"><img src="#" alt="Ralph Lauren"></div>
+				<div class="two"><img src="#" alt="DuChâteau"></div>
+			</div>
+			<hr>
+			<div class="row">
+				<div class="two"><img src="#" alt="fendly"></div>
+				<div class="two"><img src="#" alt="360 Zebra"></div>
+				<div class="two"><img src="#" alt="Lincoln"></div>
+				<div class="two"><img src="#" alt="diptyque Paris"></div>
+				<div class="two"><img src="#" alt="Only Watch"></div>
+			</div>
+		</section>
+
+		<section class="work">
+			<h3>work.</h3>
+
+			<div class="row">
+				<div class="six">
+					<img src="#" alt="#">
+					<div class="hover">
+						<p>Web Design / Branding</p>
+						<h5>Only Watch</h5>
+					</div>
+				</div>
+				<div class="six">
+					<img src="#" alt="#">
+					<div class="hover">
+						<p>Web Design / Branding</p>
+						<h5>Only Watch</h5>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="six">
+					<img src="#" alt="#">
+					<div class="hover">
+						<p>Web Design / Branding</p>
+						<h5>Only Watch</h5>
+					</div>
+				</div>
+				<div class="six">
+					<img src="#" alt="#">
+					<div class="hover">
+						<p>Web Design / Branding</p>
+						<h5>Only Watch</h5>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="news">
+			<h3>news.</h3>
+
+			<div>
+				<div class="date">December 12, 2014</div>
+				<div class="title">"A Model to Follow" : New Forrester Research Report on Oui Will</div>
+			</div>
+		</section>
+
+		<section class="carreers">
+			<h3>carreers.</h3>
+
+			<p>Ready to deliver excellence? That’s excellent!</p>
+			<a href="#" class='light-btn'>drop us a line</a>
+		</section>
+	</main>
 
 <?php snippet('footer') ?>
