9203a2c81f8fae6b46c616ca04d49f755f565f88
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-22 19:18:26 +0200

    adding in all fonts and font-face

---
+++ site/blueprints/work-detail.php
@@ -0,0 +1,22 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Work detail
+pages: false
+files:
+  sortable: true
+fields:
+  title:
+    label: Title
+    type:  text
+  year:
+    label: Year
+    type:  text
+  text:
+    label: Text
+    type:  textarea
+  tag1:
+    label: Tag 1
+    type:  text
+  tag2:
+    label: Tag 2
+    type:  text
