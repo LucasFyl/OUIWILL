22b9e66de938c11c9ec21a8ceeb421ee0c964103
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-23 18:00:10 +0200

    news section blueprint + loop home

---
+++ site/blueprints/news.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: News
+pages:
+  template: single-news
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
--- site/templates/news.php
+++ site/templates/news.php
@@ -1,12 +1,22 @@
+<?php snippet('head') ?>
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+  <main class="main" id="news">
 
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
+    <section class="list">
+      <?php foreach(page('news')->children()->visible()->limit(4) as $news): ?>
+      <div class="single-news">
+      	<a href="<?php echo $news->url() ?>">
+      		<div class="date"><?php echo $news->date() ?></div>
+      		<div class="title"><?php echo $news->title()->html() ?><span class="chevron-right"></span></div>
+      	</a>
+      </div>
+      <?php endforeach ?>
+    </section>
+    
+    <?php snippet('carreers'); ?>
 
   </main>
 
+<?php snippet('footer-big') ?>
 <?php snippet('footer') ?>
