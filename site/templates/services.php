99ece0bb3c2aa09110c908e15ae36a5dc89af0f3
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-24 13:20:03 +0200

    started workin on services most css done + few minor fixs

--- site/templates/services.php
+++ site/templates/services.php
@@ -1,12 +1,34 @@
-<?php snippet('header') ?>
+<?php snippet('head') ?>
+<?php snippet('header-black') ?>
 
-  <main class="main" role="main">
+  <main class="main" id="services">
 
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
-
+    <section class="brand">
+    	<div class="quarter">
+    		<div>
+    			<img src="<?php echo url('/assets/images/asset-service-brand-a.jpg') ?>" alt="" draggable="false" />
+    		</div>
+    	</div>
+    	<div class="quarter">
+    		<div>
+    			<img src="<?php echo url('/assets/images/asset-service-brand-b.jpg') ?>" alt="" draggable="false" />
+    		</div>
+    	</div>
+    	<div class="quarter">
+    		<div>
+    			<h3>brand.</h3>
+	    		<p>oui will helps emerging and established brands understand who they are, why they exist, and how to communicate by creating systems of expression that work across all touch points.</p>
+	    		<p class='smaller'>Brand identity / Brand Strategy / Advertising Campaigns / Video Production / Photography / Brand Collateral / Packaging Design / Product Design / Exhibition & Events.</p>
+    		</div>
+    	</div>
+    	<div class="quarter">
+    		<div>
+    			<img src="<?php echo url('/assets/images/asset-service-brand-c.jpg') ?>" alt="" draggable="false" />
+    		</div>
+    	</div>
+    </section>
+    
   </main>
 
+<?php snippet('footer-big') ?>
 <?php snippet('footer') ?>
