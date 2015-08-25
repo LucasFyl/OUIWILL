42407242631c09274da4411f34c4b7c2ea9fc8f5
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-25 15:37:52 +0200

    refactor square logic about page

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -11,10 +11,14 @@
 			</div>
 			<div class="half">
 				<div>
-					<h2>San Diego office</h2>
-					<h3>headquarter</h3>
-		    		<p>3077 North Park Way, Suite 107 <br/> San Diego, CA 92104</p>
-		    		<a href="mailto:contact@ouiwill.com" class="light-btn golden">contact@ouiwill.com</a>
+					<div class="vertical-center">
+						<div>
+							<h2>San Diego office</h2>
+							<h3>headquarter</h3>
+				    		<p>3077 North Park Way, Suite 107 <br/> San Diego, CA 92104</p>
+				    		<a href="mailto:contact@ouiwill.com" class="light-btn golden">contact@ouiwill.com</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -22,17 +26,21 @@
 		<section class="follow">
 			<div class="half">
 				<div>
-					<h2>Follow Us</h2>
-					<h3>Sharing is part of the process</h3>
-					<p>Become part of our social family. <br/>We could like and share each other.</p>
-					<div class="share-btn">
-		    			<a href="#" class="light-btn golden">twitter</a>
-		    			<a href="#" class="light-btn golden">behance</a>
-		    			<a href="#" class="light-btn golden">dribble</a> <br/>
-		    			<a href="#" class="light-btn golden">instagram</a>
-		    			<a href="#" class="light-btn golden">facebook</a>
+					<div class="vertical-center">
+						<div>
+							<h2>Follow Us</h2>
+							<h3>Sharing is part of the process</h3>
+							<p>Become part of our social family. <br/>We could like and share each other.</p>
+							<div class="share-btn">
+				    			<a href="#" class="light-btn golden">twitter</a>
+				    			<a href="#" class="light-btn golden">behance</a>
+				    			<a href="#" class="light-btn golden">dribble</a> <br/>
+				    			<a href="#" class="light-btn golden">instagram</a>
+				    			<a href="#" class="light-btn golden">facebook</a>
+							</div>
+						</div>
 					</div>
-				</div>
+				</div>		
 			</div>
 			<div class="half">
 				<div>
@@ -49,10 +57,14 @@
 			</div>
 			<div class="half">
 				<div>
-					<h2>Work For Us</h2>
-					<h3>Currently on the look for:</h3>
-					<p>Motion Designer / Front Developer / <br/> DP / Intern</p>
-		    		<a href="mailto:jobs@ouiwill.com" class="light-btn golden">jobs@ouiwill.com</a>
+					<div class="vertical-center">
+						<div>
+							<h2>Work For Us</h2>
+							<h3>Currently on the look for:</h3>
+							<p>Motion Designer / Front Developer / <br/> DP / Intern</p>
+				    		<a href="mailto:jobs@ouiwill.com" class="light-btn golden">jobs@ouiwill.com</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
