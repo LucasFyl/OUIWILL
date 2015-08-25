d4c22ec8f04dcc9e8841cb70a62adc603e605a5a
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-25 14:08:44 +0200

    page services css done

--- site/templates/services.php
+++ site/templates/services.php
@@ -1,34 +1,117 @@
 <?php snippet('head') ?>
 <?php snippet('header-black') ?>
 
-  <main class="main" id="services">
+	<main class="main" id="services">
 
-    <section class="brand">
-    	<div class="quarter">
-    		<div>
-    			<img src="<?php echo url('/assets/images/asset-service-brand-a.jpg') ?>" alt="" draggable="false" />
-    		</div>
-    	</div>
-    	<div class="quarter">
-    		<div>
-    			<img src="<?php echo url('/assets/images/asset-service-brand-b.jpg') ?>" alt="" draggable="false" />
-    		</div>
-    	</div>
-    	<div class="quarter">
-    		<div>
-    			<h3>brand.</h3>
-	    		<p>oui will helps emerging and established brands understand who they are, why they exist, and how to communicate by creating systems of expression that work across all touch points.</p>
-	    		<p class='smaller'>Brand identity / Brand Strategy / Advertising Campaigns / Video Production / Photography / Brand Collateral / Packaging Design / Product Design / Exhibition & Events.</p>
-    		</div>
-    	</div>
-    	<div class="quarter">
-    		<div>
-    			<img src="<?php echo url('/assets/images/asset-service-brand-c.jpg') ?>" alt="" draggable="false" />
-    		</div>
-    	</div>
-    </section>
-    
-  </main>
+		<section class="brand">
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-brand-a.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-brand-b.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<h3>brand.</h3>
+		    		<p>oui will helps emerging and established brands understand who they are, why they exist, and how to communicate by creating systems of expression that work across all touch points.</p>
+		    		<p class='smaller'>Brand identity / Brand Strategy / Advertising Campaigns / Video Production / Photography / Brand Collateral / Packaging Design / Product Design / Exhibition & Events.</p>
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-brand-c.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+		</section>
+
+		<section class="digital">
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-digital-a.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-digital-b.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-digital-c.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<h3>digital.</h3>
+		    		<p>we create interactive and connected digital products that achieve simplicity through a constant focus on the user’s overall experience. pixel perfect is a our religion.</p>
+		    		<p class='smaller'>Website Design / Digital Strategy / User Interface Design / User Experience Design / Social Campaings / eCommerce / Digital Installations / Web Apps / Responsive Design.</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="photo">
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-photo-a.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-photo-b.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<h3>Video & Photography.</h3>
+		    		<p>Let’s not forget that Digital Visual Merchandising is just as important as the functionality and accessibility of your web site. It represents the only language that can be understood anywhere in the world.</p>
+		    		<p class='smaller'>We have an internal team of talented photographers and video makers who have eyes like a shutter and mind like a lens. They will literally paint your distinguished photo/video canvas that will be perfectly framed into your web site.</p>
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-photo-c.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+		</section>
+
+		<section class="sectors">
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-sector-a.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-sector-b.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-service-sector-c.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="quarter">
+				<div>
+					<h3>Sectors.</h3>
+		    		<p>we combine the european esthetic and the american yes we can, creating experiences in a digital world, helping people in their decisions for a brand, products, services in different sectors.</p>
+		    		<p class='smaller'>Fashion / Arts / Automotive / Technology / Education / Health Care / Media & Technology / Profesional Services / Non-profit / Public Sctors / Restaurants / Hospitality / Sports. </p>
+				</div>
+			</div>
+		</section>
+
+	</main>
+
+  	<div class="blockquote">
+		<img src="<?php echo url('/assets/images/asset-blockquote-onlywatch.jpg'); ?>" alt="OnlyWatch" />
+		<div>
+			<p class="quote">« we enjoy oui will's enthusiasm to discover, <br/> create and actualize ».</p>
+			<p class="author">Luc Pettavino / President of Only Watch</p>
+		</div>
+	</div>
 
 <?php snippet('footer-big') ?>
 <?php snippet('footer') ?>
