7bfabdbc4bfb0f3ef96e36ee2221f1adcebc0e71
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-25 15:06:30 +0200

    page contact css done

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,12 +1,63 @@
-<?php snippet('header') ?>
+<?php snippet('head') ?>
+<?php snippet('header-black') ?>
 
-  <main class="main" role="main">
+	<main class="main" id="contact">
 
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
+		<section class="office">
+			<div class="half">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-contact-a.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="half">
+				<div>
+					<h2>San Diego office</h2>
+					<h3>headquarter</h3>
+		    		<p>3077 North Park Way, Suite 107 <br/> San Diego, CA 92104</p>
+		    		<a href="mailto:contact@ouiwill.com" class="light-btn golden">contact@ouiwill.com</a>
+				</div>
+			</div>
+		</section>
 
-  </main>
+		<section class="follow">
+			<div class="half">
+				<div>
+					<h2>Follow Us</h2>
+					<h3>Sharing is part of the process</h3>
+					<p>Become part of our social family. <br/>We could like and share each other.</p>
+					<div class="share-btn">
+		    			<a href="#" class="light-btn golden">twitter</a>
+		    			<a href="#" class="light-btn golden">behance</a>
+		    			<a href="#" class="light-btn golden">dribble</a> <br/>
+		    			<a href="#" class="light-btn golden">instagram</a>
+		    			<a href="#" class="light-btn golden">facebook</a>
+					</div>
+				</div>
+			</div>
+			<div class="half">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-contact-b.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+		</section>
 
+		<section class="jobs">
+			<div class="half">
+				<div>
+					<img src="<?php echo url('/assets/images/asset-contact-c.jpg') ?>" alt="" draggable="false" />
+				</div>
+			</div>
+			<div class="half">
+				<div>
+					<h2>Work For Us</h2>
+					<h3>Currently on the look for:</h3>
+					<p>Motion Designer / Front Developer / <br/> DP / Intern</p>
+		    		<a href="mailto:jobs@ouiwill.com" class="light-btn golden">jobs@ouiwill.com</a>
+				</div>
+			</div>
+		</section>
+
+	</main>
+
+<?php snippet('footer-big') ?>
 <?php snippet('footer') ?>
