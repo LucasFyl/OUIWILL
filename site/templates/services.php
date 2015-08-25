9dbb314547ad3884f61b1a9f159bece68b28ccf4
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-25 15:43:56 +0200

    refactor square logic and its centered content

--- site/templates/services.php
+++ site/templates/services.php
@@ -16,9 +16,13 @@
 			</div>
 			<div class="quarter">
 				<div>
-					<h3>brand.</h3>
-		    		<p>oui will helps emerging and established brands understand who they are, why they exist, and how to communicate by creating systems of expression that work across all touch points.</p>
-		    		<p class='smaller'>Brand identity / Brand Strategy / Advertising Campaigns / Video Production / Photography / Brand Collateral / Packaging Design / Product Design / Exhibition & Events.</p>
+					<div class="vertical-center">
+						<div>
+							<h3>brand.</h3>
+			    			<p>oui will helps emerging and established brands understand who they are, why they exist, and how to communicate by creating systems of expression that work across all touch points.</p>
+			    			<p class='smaller'>Brand identity / Brand Strategy / Advertising Campaigns / Video Production / Photography / Brand Collateral / Packaging Design / Product Design / Exhibition & Events.</p>	
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="quarter">
@@ -46,9 +50,14 @@
 			</div>
 			<div class="quarter">
 				<div>
-					<h3>digital.</h3>
-		    		<p>we create interactive and connected digital products that achieve simplicity through a constant focus on the user’s overall experience. pixel perfect is a our religion.</p>
-		    		<p class='smaller'>Website Design / Digital Strategy / User Interface Design / User Experience Design / Social Campaings / eCommerce / Digital Installations / Web Apps / Responsive Design.</p>
+					<div class="vertical-center">
+						<div>
+							<h3>digital.</h3>
+			    			<p>we create interactive and connected digital products that achieve simplicity through a constant focus on the user’s overall experience. pixel perfect is a our religion.</p>
+				    		<p class='smaller'>Website Design / Digital Strategy / User Interface Design / User Experience Design / Social Campaings / eCommerce / Digital Installations / Web Apps / Responsive Design.</p>	
+						</div>
+					</div>
+						
 				</div>
 			</div>
 		</section>
@@ -66,9 +75,13 @@
 			</div>
 			<div class="quarter">
 				<div>
-					<h3>Video & Photography.</h3>
-		    		<p>Let’s not forget that Digital Visual Merchandising is just as important as the functionality and accessibility of your web site. It represents the only language that can be understood anywhere in the world.</p>
-		    		<p class='smaller'>We have an internal team of talented photographers and video makers who have eyes like a shutter and mind like a lens. They will literally paint your distinguished photo/video canvas that will be perfectly framed into your web site.</p>
+					<div class="vertical-center">
+						<div>
+							<h3>Video & Photography.</h3>
+			    			<p>Let’s not forget that Digital Visual Merchandising is just as important as the functionality and accessibility of your web site. It represents the only language that can be understood anywhere in the world.</p>
+			    			<p class='smaller'>We have an internal team of talented photographers and video makers who have eyes like a shutter and mind like a lens. They will literally paint your distinguished photo/video canvas that will be perfectly framed into your web site.</p>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="quarter">
@@ -96,9 +109,13 @@
 			</div>
 			<div class="quarter">
 				<div>
-					<h3>Sectors.</h3>
-		    		<p>we combine the european esthetic and the american yes we can, creating experiences in a digital world, helping people in their decisions for a brand, products, services in different sectors.</p>
-		    		<p class='smaller'>Fashion / Arts / Automotive / Technology / Education / Health Care / Media & Technology / Profesional Services / Non-profit / Public Sctors / Restaurants / Hospitality / Sports. </p>
+					<div class="vertical-center">
+						<div>
+							<h3>Sectors.</h3>
+			    			<p>we combine the european esthetic and the american yes we can, creating experiences in a digital world, helping people in their decisions for a brand, products, services in different sectors.</p>
+			    			<p class='smaller'>Fashion / Arts / Automotive / Technology / Education / Health Care / Media & Technology / Profesional Services / Non-profit / Public Sctors / Restaurants / Hospitality / Sports. </p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
