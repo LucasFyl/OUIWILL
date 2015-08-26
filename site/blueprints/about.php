04a2db571e55632584d2b5c450f2c653f1b8fffb
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-26 16:47:15 +0200

    dynamic content about page

---
+++ site/blueprints/about.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: About
+pages: false
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
+  vimeoLink:
+    label: Vimeo link for the video
+    type:  url
