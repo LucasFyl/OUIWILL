11f526532906d6f05cd00b06ff41277d8d372ba4
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-27 20:58:39 +0200

    fix files permission + gitignore

---
+++ site/accounts/admin.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+email: lucas.fayolle@orange.fr
+password: >
+  $2a$10$QGZ.OY2ZGMKjgt3700WZIeqORArp1lP59TZQgBZq6DB92R0coHK7.
+language: en
+role: admin
+token: 30685c7ac5e810bdac94fef022f0ea34d3bb544e
+history:
+  - work/go-haus
+  - work/mister-brown
+  - work/openeyeglobal
+  - work/onlywatch
+  - work
