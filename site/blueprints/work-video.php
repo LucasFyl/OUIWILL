d34f453f6accc342b4816d309a25dbbcda4bca58
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-27 16:49:15 +0200

    lots of work on video&photo template + video management backend + modal video link mgmt php

--- site/blueprints/work-video.php
+++ site/blueprints/work-video.php
@@ -5,19 +5,19 @@ pages: false
 files:
   sortable: true
 fields:
+  generalinfo:
+    label: General Information
+    type: headline
   title:
-    label: Project Title
+    label: Project Title (homepage work page and landing subtitle)
     type:  text
   h1:
-    label: Titre landing
+    label: Landing title
     type:  text
-    default: Video
-  subtitle:
-    label: Subtitle
-    type:  text
-  text:
-    label: Text
-    type:  textarea
+    default: Video & Photography
+  sidebarcontent:
+    label: Sidebar Content
+    type: headline
   client:
     label: Client
     type:  text
@@ -27,3 +27,63 @@ fields:
   website:
     label: Website url
     type:  url
+  casestudycontent:
+    label: Case Study Content
+    type: headline
+  subtitle:
+    label: Case Study title
+    type:  text
+  text:
+    label: Case Study text
+    type:  textarea
+  info3:
+    label: Assets
+    type: headline
+  vimeo1:
+    label: Vimeo url 1
+    type:  url
+    width: 1/2
+  vimeocaption1:
+    label: Vimeo caption 1 (optional)
+    type:  text
+    width: 1/2
+  vimeo2:
+    label: Vimeo url 2
+    type:  url
+    width: 1/2
+  vimeocaption2:
+    label: Vimeo caption 2 (optional)
+    type:  text
+    width: 1/2
+  vimeo3:
+    label: Vimeo url 3
+    type:  url
+    width: 1/2
+  vimeocaption3:
+    label: Vimeo caption 3 (optional)
+    type:  text
+    width: 1/2
+  vimeo4:
+    label: Vimeo url 4
+    type:  url
+    width: 1/2
+  vimeocaption4:
+    label: Vimeo caption 4 (optional)
+    type:  text
+    width: 1/2
+  vimeo5:
+    label: Vimeo url 5
+    type:  url
+    width: 1/2
+  vimeocaption5:
+    label: Vimeo caption 5 (optional)
+    type:  text
+    width: 1/2
+  vimeo6:
+    label: Vimeo url 5
+    type:  url
+    width: 1/2
+  vimeocaption6:
+    label: Vimeo caption 5 (optional)
+    type:  text
+    width: 1/2
