7209af6ebaf7f5331e049e476821a3f6d47d874a
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-29 19:57:09 +0200

    fix nextprev work video

--- site/templates/work-video.php
+++ site/templates/work-video.php
@@ -75,16 +75,6 @@
         <?php $i++ ?>
       <?php endforeach ?>
     </div>
-
-    <nav class="nextprev cf" role="navigation">
-      <?php if($prev = $page->prevVisible()): ?>
-      <a class="prev" href="<?php echo $prev->url() ?>">&larr; previous</a>
-      <?php endif ?>
-      <?php if($next = $page->nextVisible()): ?>
-      <a class="next" href="<?php echo $next->url() ?>">next &rarr;</a>
-      <?php endif ?>
-    </nav>
-
   </main>
 <?php snippet('work-next') ?>
 
