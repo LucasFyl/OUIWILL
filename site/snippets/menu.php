c0d6b54997b8c9bec02deb3369caa3ea22440364
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-29 14:22:49 +0200

    onclick close menu function + icon

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,15 +1,17 @@
 <div id="trigger"><i class="fa fa-bars"></i></div>
 <div class="navigation" id="menu">
-  <div class="brand">
-    <h1><img src="<?php echo url('assets/images/logo-black.png') ?>" alt="<?php echo $site->title()->html() ?>" draggable="false"/></h1>
-    <h2 class="baseline">precisely what you imagined your brand could be only more so. ™</h2>
-    <div class="social">
-      <ul>
-        <li><a href="#" class="fa fa-vimeo"></a></li>
-        <li><a href="#" class="fa fa-behance"></a></li>
-        <li><a href="#" class="fa fa-facebook"></a></li>
-        <li><a href="#" class="fa fa-instagram"></a></li>
-      </ul>
+  <div class="brand-wrap">
+    <div class="brand">
+      <h1><img src="<?php echo url('assets/images/logo-black.png') ?>" alt="<?php echo $site->title()->html() ?>" draggable="false"/></h1>
+      <h2 class="baseline">precisely what you imagined your brand could be only more so. ™</h2>
+      <div class="social">
+        <ul>
+          <li><a href="#" class="fa fa-vimeo"></a></li>
+          <li><a href="#" class="fa fa-behance"></a></li>
+          <li><a href="#" class="fa fa-facebook"></a></li>
+          <li><a href="#" class="fa fa-instagram"></a></li>
+        </ul>
+      </div>
     </div>
   </div>
   <div class="menu">
