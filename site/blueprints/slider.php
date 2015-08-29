155f4db328912a38fd44bc6fa14ba046520d25e2
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-29 14:52:32 +0200

    slider content BO + slider bg css

---
+++ site/blueprints/slider.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Slider
+pages: true
+files: false
+deletable: false
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
