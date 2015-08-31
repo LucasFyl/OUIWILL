82221163356609cd7bf20876839283653b0cae42
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-31 17:01:29 +0200

    adding in js components and home slider css

---
+++ assets/js/components/navigation.js
@@ -0,0 +1,48 @@
+(function(){
+	
+	var Navigation = {
+		init: function(menutimeline){
+			TweenMax.set('#menu', {display:'none',opacity:0});
+			TweenMax.set('#menu .brand > *', {opacity:0,y:20});
+			TweenMax.set('#menu .menu', {opacity:0,x:'100%'});
+			TweenMax.set('#menu .menu li', {opacity:0,y:20});
+			
+			var menutimeline = new TimelineMax({paused:true});
+			menutimeline.to('#menu', 0.45, {display:'block',opacity:1,ease:Power2.easeIn})
+						.to('#menu .menu', 0.45, {opacity:1,x:'0%',ease:Power2.easeOut})
+						.staggerTo('#menu .brand > *', 0.5, {opacity:1,y:0,ease:Power2.easeOut}, 0.1)
+						.staggerTo('#menu .menu li', 0.5, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.4");
+
+			this.bindEvents(menutimeline);
+			return menutimeline;
+		},
+
+		openMenu: function(menutimeline){
+			TweenMax.set('body', {overflow:'hidden'});
+			menutimeline.play();
+		},
+		closeMenu: function(menutimeline){
+			menutimeline.reverse();
+		  	setTimeout(function(){
+				TweenMax.set('body', {overflow:'auto'});
+		  	}, 2100);
+		},
+
+		bindEvents: function(menutimeline){
+			$(document).on('click', '#trigger', function(){
+				Navigation.openMenu(menutimeline);
+			});
+			$(document).keyup(function(e){
+				if(e.which == 27) {
+				  	Navigation.closeMenu(menutimeline);
+				}
+			});
+			$(document).on('click', '.brand-wrap', function(){
+				Navigation.closeMenu(menutimeline);
+			});
+		}
+	}
+
+	Navigation.init();
+
+})();
