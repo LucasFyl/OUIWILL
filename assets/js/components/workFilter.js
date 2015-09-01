d35e66bb62420ec99015b89a011a12673a6ccab5
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-01 17:48:55 +0200

    work page filter working + php random link(

---
+++ assets/js/components/workFilter.js
@@ -0,0 +1,30 @@
+(function(){
+	var WorkFilter = {
+		init:function(){
+			// init filters
+			this.bindEvents();
+		},
+		filter: function(_this){
+			var needed = '.single-work.'+_this;
+			var notneeded = '.single-work:not(.'+_this+')';
+			TweenMax.staggerTo(notneeded, 1, {y:200,opacity:0,ease:Power4.easeInOut,onComplete:WorkFilter.updateGrid(needed, notneeded)}, 0.1);
+		},
+		reset: function(clickTarget){
+			var _this = $(this).attr('data-filter');
+			$('.single-work').removeClass('disabled');
+			if ( _this != 'all' ) { WorkFilter.filter(_this); } else {
+				TweenMax.staggerFromTo('.single-work', 1, {opacity:0,y:20}, {y:0,opacity:1,ease:Power4.easeInOut}, 0.15);
+			}
+		},
+		updateGrid: function(needed, notneeded){
+			// hide not needed 
+			$('.showcase').find(notneeded).addClass('disabled');
+			TweenMax.staggerFromTo(needed, 1, {opacity:0,y:20}, {y:0,opacity:1,ease:Power4.easeInOut}, 0.15);
+		},
+		bindEvents: function(){
+			// bind click events
+			$('body').on("click", "#filters a.filter", WorkFilter.reset );
+		}
+	}
+	WorkFilter.init();
+})();
--- site/templates/work.php
+++ site/templates/work.php
@@ -4,28 +4,40 @@
   <main class="main" id="work">
 
     <div class="filterpart">
-      <div>
-        <p>Filter by: <a href="#" class="active">all project</a><a href="#">digital</a><a href="#">video</a><a href="#">photography</a></p>
+      <div id="filters">
+        <p>Filter by: 
+          <a href="#" class="active filter" data-filter="all">all project</a>
+          <a href="#" class="filter" data-filter="digital">digital</a>
+          <a href="#" class="filter" data-filter="video">video</a>
+          <a href="#" class="filter" data-filter="photography">photography</a></p>
       </div>
-      <div><a href="#">try random <span></span></a></div>
+      <div>
+        <div class="hide">
+          <?php echo $nbr = $page->children()->count(); ?>
+          <?php echo $target = rand(0, $nbr) ?>
+          <?php echo $randomWork = $page->children()->nth($target)->url(); ?>
+        </div>
+        <a href="<?php echo $randomWork ?>">try random <span></span></a>
+      </div>
     </div>
 
     <section class="showcase">
     <!-- Add limit 11 when time -->
       <?php foreach(page('work')->children()->visible() as $project): ?>
-      <div class="single-work">
+      <div class="single-work <?php echo $project->categories() ?>">
         <a href="<?php echo $project->url() ?>"></a>
         <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
-        <img src="<?php echo $image->url() ?>" alt="#">
+        <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title() ?>">
         <?php endif ?>
 
-        <div class="hover">
+        <div class="hover"> 
           <div class="vert-cent">
             <h5><?php echo $project->title()->html() ?></h5>
             
             <?php if (!$project->tag1()->isEmpty()): ?>
             <p><?php echo $project->tag1()->html() ?> / <?php echo $project->tag2()->html() ?></p>
             <?php endif ?>
+
             <?php if (!$project->h1()->isEmpty()): ?>
             <p><?php echo $project->h1()->html() ?></p>
             <?php endif ?>
@@ -44,4 +56,5 @@
   </main>
 
 <?php snippet('footer-big') ?>
+<?= js('assets/js/components/workFilter.js') ?>
 <?php snippet('footer') ?>
