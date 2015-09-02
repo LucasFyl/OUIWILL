b1eb30d6266ab070971514ad2906e3700d1ea59b
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-02 15:36:13 +0200

    sidebar small fix

--- assets/js/scrollControl-WorkDetail.js
+++ assets/js/scrollControl-WorkDetail.js
@@ -4,6 +4,7 @@ var tlnextprev = new TimelineMax({paused:true});
 // var sideBarTween = new TweenMax.set('#sidebar', {className:'+=fixed'});
 
 tlnextprev.set('.txt-wrap > *', {opacity:0,y:20})
+		.set('#sidebar', {className:'-=fixed'})
 		.fromTo('.nextprev', 0.5, {yPercent:100,zIndex:-1}, {yPercent:0,zIndex:9,ease:Power4.easeInOut})
 		.staggerTo('.prev .txt-wrap > *', 0.25, {opacity:1,y:0,ease:Power3.easeOut}, 0.1)
 		.staggerTo('.next .txt-wrap > *', 0.25, {opacity:1,y:0,ease:Power3.easeOut}, 0.1, "-=0.5");
@@ -12,6 +13,7 @@ tlnextprev.set('.txt-wrap > *', {opacity:0,y:20})
 var nextPrev = new ScrollMagic.Scene({triggerElement: "footer.big", triggerHook: 'onEnter'})
     .addTo(controller)
     .setTween(tlnextprev.play());
+
 var sideBar = new ScrollMagic.Scene({triggerElement: "#sidebar", triggerHook: 'onLeave', offset:-60, duration:0})
     .addTo(controller)
     .setClassToggle("#sidebar", "fixed");
