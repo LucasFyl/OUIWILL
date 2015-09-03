da9e7d74275a438fa7c4d5c28a6ea16dd99017b0
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-03 16:01:32 +0200

    javascript mobile detection

--- assets/js/scrollControl-WorkDetail.js
+++ assets/js/scrollControl-WorkDetail.js
@@ -2,6 +2,14 @@ var controller = new ScrollMagic.Controller();
 
 var tlnextprev = new TimelineMax({paused:true});
 // var sideBarTween = new TweenMax.set('#sidebar', {className:'+=fixed'});
+console.log(isMobile);
+if ( isMobile = false) {
+	console.log(isMobile);
+	var sideBar = new ScrollMagic.Scene({triggerElement: "#sidebar", triggerHook: 'onLeave', offset:-60, duration:0})
+	    .addTo(controller)
+	    .setClassToggle("#sidebar", "fixed");
+}
+
 
 tlnextprev.set('.txt-wrap > *', {opacity:0,y:20})
 		.set('#sidebar', {className:'-=fixed'})
@@ -9,12 +17,6 @@ tlnextprev.set('.txt-wrap > *', {opacity:0,y:20})
 		.staggerTo('.prev .txt-wrap > *', 0.25, {opacity:1,y:0,ease:Power3.easeOut}, 0.1)
 		.staggerTo('.next .txt-wrap > *', 0.25, {opacity:1,y:0,ease:Power3.easeOut}, 0.1, "-=0.5");
 		
-
 var nextPrev = new ScrollMagic.Scene({triggerElement: "footer.big", triggerHook: 'onEnter'})
     .addTo(controller)
     .setTween(tlnextprev.play());
-
-var sideBar = new ScrollMagic.Scene({triggerElement: "#sidebar", triggerHook: 'onLeave', offset:-60, duration:0})
-    .addTo(controller)
-    .setClassToggle("#sidebar", "fixed");
-    // .setTween(sideBarTween);
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,7 +3,6 @@
 <?= js('assets/js/vendor/jquery.hoverIntent.min.js') ?>
 <?= js('assets/js/components/navigation.js') ?>
 <?= js('assets/js/components/modal.js') ?>
-<?= js('assets/js/app.js') ?>
 
 <!-- build:remove -->
 <script src="http://localhost:35729/livereload.js"></script>
