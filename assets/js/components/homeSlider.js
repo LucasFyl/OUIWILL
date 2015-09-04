487d9424d155e27e1752aeada58f18494c8ddc9c
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-04 12:47:26 +0200

    kill slider loop after 4 occurence

--- assets/js/components/homeSlider.js
+++ assets/js/components/homeSlider.js
@@ -3,6 +3,7 @@
 		init:function(){
 			// define slider components
 			var val = 0;
+			var foo = 0;
 			var content = $('#mySlider .slider-content ul li');
 			var kids = content.children('h2, p, a');
 			var nav = $('#mySlider .slider-nav .nav-item');
@@ -20,10 +21,9 @@
 			});
 
 			// set components positions 
-			this.reset(content, kids, images, nav, val);
-
+			this.reset(content, kids, images, nav, val, foo);
 		},
-		reset: function(content, kids, images, nav, val){
+		reset: function(content, kids, images, nav, val, foo){
 			// (re)set slide content style for animation
 			TweenMax.set(kids, {opacity:0,y:20});
 			TweenMax.set(images, {zIndex:2,opacity:0});
@@ -39,9 +39,9 @@
 			// trigger navigation
 			this.navigation(val);
 			// trigger slider
-			this.slide(content, kids, images, nav, val);
+			this.slide(content, kids, images, nav, val, foo);
 		},
-		navigation: function(val){
+		navigation: function(val, foo){
 			var targetNav = $('#mySlider .slider-nav .nav-item.i_' + val);
 			var targetDot = targetNav.find('.nav-link');
 			var targetBar = targetNav.find('.bar');
@@ -58,36 +58,49 @@
 				TweenMax.set([targetBar,targetDot], {className:'+=active'});
 			}
 		},
-		slide: function(content, kids, images, nav, val){
-			setInterval(function(){
-				
-				// if 0 then target is 1
-				if (val === 0) {val=val+1;}
-				
-				// if not last slide switch to next, else switch to first an reset val
-				if( val <= 3 ) {
-					// stop all buffered events and execute only last animation
+		slide: function(content, kids, images, nav, val, foo){
+			console.log('foo:',foo);
+			var slideInt = setInterval(function(){
+				if ( foo <= 4) { 
+					// killer variable
+					foo = foo+1;
+					
+					// if 0 then target is 1
+					if (val === 0) {val=val+1;foo = foo+1;}
+
+					// if not last slide switch to next, else switch to first an reset val
+					if( val <= 3 ) {
+						// stop all buffered events and execute only last animation
+						HomeSlider.stopBuffering(content, kids, images, nav);
+						// slide to target, update nav, and increment slide val
+						HomeSlider.slideTo(val);
+						HomeSlider.navigation(val);
+						val=val+1;
+					console.log("foo:",foo);
+					console.log("val:",val);
+						return;
+					} else {
+						// stop all buffered events and execute only last animation
+						HomeSlider.stopBuffering(content, kids, images, nav);
+
+						// reset everything and start again
+						val = 0;
+					console.log("foo:",foo);
+					console.log("val:",val);
+						HomeSlider.reset(content, kids, images, nav, val);
+						return;
+					}
+				} else { 
+					clearInterval(slideInt);
+					val = 0; foo = 0;
 					HomeSlider.stopBuffering(content, kids, images, nav);
-					// slide to target, update nav, and increment slide val
-					HomeSlider.slideTo(val);
-					HomeSlider.navigation(val);
-					val=val+1;
-					return;
-				} else {
-					// stop all buffered events and execute only last animation
-					HomeSlider.stopBuffering(content, kids, images, nav);
-					// reset everything and start again
-					val = 0;
-					HomeSlider.reset(content, kids, images, nav, val);
-					return;
+					HomeSlider.kill(content, kids, images, nav, val, foo); 
 				}
-				return;
-			}, 5000 );
+			}, 5000);
 		},
-		slideTo: function(val){
+		slideTo: function(val, foo){
 			var targetContent = $('#mySlider .slider-content li.i_' + val).children('h2, p, a');
 			var targetImage = $('#mySlider .slider-bg img.i_' + val);
-
 			TweenMax.staggerTo(targetContent, 0.5, {opacity:1,y:0,ease:Power2.easeOut,onComplete:function(){
 				TweenMax.to(targetContent, 0.25, {y:20,opacity:0,ease:Power2.easeIn,delay:4});
 			}}, 0.25);
@@ -95,7 +108,7 @@
 		},
 		stopBuffering: function(content, kids, images, nav){
 			// stop all buffered events and execute only last animation
-			// fix the inactive-tab bug as seen on : 
+			// MAY fix the inactive-tab bug as seen on : 
 			// http://stackoverflow.com/questions/6183463/when-using-setinterval-if-i-switch-tabs-in-chrome-and-go-back-the-slider-goes
 			$('#mySlider').stop(true,true);
 			$('#mySlider .slider-content').stop(true,true);
@@ -105,6 +118,23 @@
 			kids.stop(true,true);
 			images.stop(true,true);
 			nav.stop(true,true);
+		},
+		kill: function(content, kids, images, nav, val, foo){
+			console.log('kill');
+			// (re)set slide content style for animation
+			TweenMax.set(kids, {opacity:0,y:20});
+			TweenMax.set(images, {zIndex:2,opacity:0});
+			// init first slide positions
+			var firstContent = $('li.i_0').children('h2, p, a');
+			var firstImage = $('img.i_0');
+			TweenMax.to(firstImage, 0.25, {zIndex:3,opacity:1,ease:Power2.easeOut});
+			TweenMax.staggerTo(firstContent, 0.5, {opacity:1,y:0,ease:Expo.easeOut,delay:0.5,onComplete:function(){
+				TweenMax.to(firstContent, 0.25, {y:20,opacity:0,ease:Power2.easeIn,delay:3.5});
+			}}, 0.15);
+			// reset navigation components
+			TweenMax.set('.bar', {className:"+=still"});
+			TweenMax.set(['.nav-link','.bar'], {className:'-=active',delay:0.01});
+			TweenMax.set('.bar', {className:"-=still",delay:0.02});
 		},
 		bindEvent: function(){
 			$('body').on('click', '.nav-link', function(e){
