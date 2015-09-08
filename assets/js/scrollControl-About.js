24710073693c9aef5402805e26e5cdc4f872ffb3
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-08 12:55:50 +0200

    page about responsive

--- assets/js/scrollControl-About.js
+++ assets/js/scrollControl-About.js
@@ -1,21 +1,25 @@
-var controller = new ScrollMagic.Controller();
-var founderImgTween = new TweenMax.fromTo('#about .founder img', 1, {opacity:0}, {opacity:1,ease:Power4.easeInOut});
-var founderTxtTween = new TweenMax.staggerFromTo('#about .founder h2, #about .founder h3, #about .founder p', 1, {opacity:0,y:20}, {opacity:1,y:0,ease:Power4.easeInOut}, 0.1);
-var parisSandiegoTween = new TweenMax.fromTo('#about .paris-sandiego .row', 1, {opacity:0}, {opacity:1,ease:Power4.easeInOut});
-var carreersTween = new TweenMax.fromTo('.carreers .light-btn', 0.25, {opacity:0,scale:0.6}, {opacity:1,scale:1,ease:Elastic.easeInOut});
+$(document).ready(function(){
+	var controller = new ScrollMagic.Controller();
+	var founderImgTween = new TweenMax.fromTo('#about .founder img', 1, {opacity:0}, {opacity:1,ease:Power4.easeInOut});
+	var founderTxtTween = new TweenMax.staggerFromTo('#about .founder h2, #about .founder h3, #about .founder p', 1, {opacity:0,y:20}, {opacity:1,y:0,ease:Power4.easeInOut}, 0.1);
+	var parisSandiegoTween = new TweenMax.fromTo('#about .paris-sandiego .row', 1, {opacity:0}, {opacity:1,ease:Power4.easeInOut});
 
-var founderImg = new ScrollMagic.Scene({triggerElement: "#about .founder", triggerHook: 'onEnter'})
-    .addTo(controller)
-    .setTween(founderImgTween);
+	var founderImg = new ScrollMagic.Scene({triggerElement: "#about .founder", triggerHook: 'onEnter'})
+	    .addTo(controller)
+	    .setTween(founderImgTween);
 
-var founderTxt = new ScrollMagic.Scene({triggerElement: "#about .founder .row", triggerHook: 'onEnter'})
-    .addTo(controller)
-    .setTween(founderTxtTween);
+	var founderTxt = new ScrollMagic.Scene({triggerElement: "#about .founder .row", triggerHook: 'onEnter'})
+	    .addTo(controller)
+	    .setTween(founderTxtTween);
 
-var parisSandiego = new ScrollMagic.Scene({triggerElement: "#about .paris-sandiego", triggerHook: 'onEnter'})
-    .addTo(controller)
-    .setTween(parisSandiegoTween);
+	var parisSandiego = new ScrollMagic.Scene({triggerElement: "#about .paris-sandiego", triggerHook: 'onEnter'})
+	    .addTo(controller)
+	    .setTween(parisSandiegoTween);
 
-var carreersScene = new ScrollMagic.Scene({triggerElement: ".carreers p"})
-    .addTo(controller)
-    .setTween(carreersTween);
+	if (isMobile === false){
+		var carreersTween = new TweenMax.fromTo('.carreers .light-btn', 0.25, {opacity:0,scale:0.6}, {opacity:1,scale:1,ease:Elastic.easeInOut});
+		var carreersScene = new ScrollMagic.Scene({triggerElement: ".carreers p"})
+		    .addTo(controller)
+		    .setTween(carreersTween);
+	} 
+});
