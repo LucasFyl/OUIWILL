d6056eb78b469c0d9434aaa2562d95dcbde9551a
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-11 18:25:08 +0200

    news prevnext fix

--- site/snippets/news-next.php
+++ site/snippets/news-next.php
@@ -1,56 +1,57 @@
-<nav class="nextprev cf news-nav" role="navigation">
-	<?php if( $prev = $page->prevVisible() && $next = $page->nextVisible() ): ?>
-  	<div class="blockprevnext half prev">
-  		<div class="txt-wrap">
-			<a href="<?php echo $page->prev()->url() ?>"><span class="arrow prev"></span></a>
-			<p><a href="<?php echo $page->prev()->url() ?>">Previous news</a></p>
-			<h4><a href="<?php echo $page->prev()->url() ?>"><?php echo $page->prev()->title()->excerpt(42) ?></a></h4>
-  		</div>
-  		<div>
-			<a href="<?php echo $page->prev()->url() ?>"></a>
-			<div class="overlay"></div>
-  			<img src="<?php echo $page->prev()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->prev()->title() ?>">
-  		</div>
-  	</div>
-  	<div class="blockprevnext half next">
-  		<div class="txt-wrap">
-			<a href="<?php echo $page->next()->url() ?>"><span class="arrow next"></span></a>
-			<p><a href="<?php echo $page->next()->url() ?>">Next news</a></p>
-			<h4><a href="<?php echo $page->next()->url() ?>"><?php echo $page->next()->title()->excerpt(42) ?></a></h4>
-  		</div>
-  		<div>
-			<a href="<?php echo $page->next()->url() ?>"></a>
-			<div class="overlay"></div>
-  			<img src="<?php echo $page->next()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->next()->title() ?>">
-  		</div>
-  	</div>
- 	<?php elseif( $prev = !$page->prevVisible() ):  ?>
-	<div class="blockprevnext full next">
-		<div class="txt-wrap">
-			<a href="<?php echo $page->next()->url() ?>"><span class="arrow next"></span></a>
-			<p><a href="<?php echo $page->next()->url() ?>">Next news</a></p>
-			<h4><a href="<?php echo $page->next()->url() ?>"><?php echo $page->next()->title()->excerpt(35) ?></a></h4>
-  		</div>
-  		<div>
-			<a href="<?php echo $page->next()->url() ?>"></a>
-			<div class="overlay"></div>
-  			<img src="<?php echo $page->next()->image('02-landing.jpg')->url() ?>" alt="<?php echo $page->next()->title() ?>" class="hide-mobile" />
-  			<img src="<?php echo $page->next()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->next()->title() ?>" class="hide-desktop" />
-  		</div>
+<nav class="nextprev newsnav" role="navigation">
+	<div class="blockprevnext">
+
+		<?php if( $prev = $page->prevVisible() && $next = $page->nextVisible() ): ?>
+	  	<div class="half prev">
+	  		<div class="txt-wrap">
+				<p><a href="<?php echo $page->prev()->url() ?>">Previous news</a></p>
+				<h4><a href="<?php echo $page->prev()->url() ?>"><span class="arrow prevArrow"></span> <?php echo $page->prev()->title()->excerpt(35) ?></a></h4>
+	  		</div>
+	  		<div>
+				<a href="<?php echo $page->prev()->url() ?>" class="hoverTrigger prev"></a>
+				<div class="overlay"></div>
+	  			<img src="<?php echo $page->prev()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->prev()->title()->excerpt(35) ?>">
+	  		</div>
+	  	</div>
+	  	<div class="half next">
+	  		<div class="txt-wrap">
+				<p><a href="<?php echo $page->next()->url() ?>">Next news</a></p>
+				<h4><a href="<?php echo $page->next()->url() ?>"><span class="arrow nextArrow"></span> <?php echo $page->next()->title()->excerpt(35) ?></a></h4>
+	  		</div>
+	  		<div>
+				<a href="<?php echo $page->next()->url() ?>" class="hoverTrigger next"></a>
+				<div class="overlay"></div>
+	  			<img src="<?php echo $page->next()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->next()->title()->excerpt(35) ?>">
+	  		</div>
+	  	</div>
+
+	 	<?php elseif( $prev = !$page->prevVisible() ):  ?>
+		<div class="full next">
+			<div class="txt-wrap">
+				<p><a href="<?php echo $page->next()->url() ?>">Next news</a></p>
+				<h4><a href="<?php echo $page->next()->url() ?>"><span class="arrow nextArrow"></span> <?php echo $page->next()->title()->excerpt(35) ?></a></h4>
+	  		</div>
+	  		<div>
+				<a href="<?php echo $page->next()->url() ?>" class="hoverTrigger next"></a>
+				<div class="overlay"></div>
+	  			<img src="<?php echo $page->next()->image('02-landing.jpg')->url() ?>" alt="<?php echo $page->next()->title()->excerpt(35) ?>" class="hide-mobile" />
+	  			<img src="<?php echo $page->next()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->next()->title()->excerpt(35) ?>" class="hide-desktop" />
+	  		</div>
+		</div>
+
+		<?php elseif( $next = !$page->nextVisible() ): ?>
+		<div class="full prev">
+			<div class="txt-wrap">
+				<p><a href="<?php echo $page->prev()->url() ?>">Previous news</a></p>
+				<h4><a href="<?php echo $page->prev()->url() ?>"><span class="arrow prevArrow"></span> <?php echo $page->prev()->title()->excerpt(35) ?></a></h4>
+	  		</div>
+	  		<div>
+				<a href="<?php echo $page->prev()->url() ?>" class="hoverTrigger prev"></a>
+				<div class="overlay"></div>
+	  			<img src="<?php echo $page->prev()->image('02-landing.jpg')->url() ?>" alt="<?php echo $page->prev()->title()->excerpt(35) ?>" class="hide-mobile" />
+	  			<img src="<?php echo $page->prev()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->prev()->title()->excerpt(35) ?>" class="hide-desktop" />
+	  		</div>
+		</div>
+		<?php endif ?>
 	</div>
-	<?php elseif( $next = !$page->nextVisible() ): ?>
-	<div class="blockprevnext full prev">
-		<div class="txt-wrap">
-			<a href="<?php echo $page->prev()->url() ?>"><span class="arrow prev"></span></a>
-			<p><a href="<?php echo $page->prev()->url() ?>">Previous news</a></p>
-			<h4><a href="<?php echo $page->prev()->url() ?>"><?php echo $page->prev()->title()->excerpt(35) ?></a></h4>
-  		</div>
-  		<div>
-			<a href="<?php echo $page->prev()->url() ?>"></a>
-			<div class="overlay"></div>
-  			<img src="<?php echo $page->prev()->image('02-landing.jpg')->url() ?>" alt="<?php echo $page->prev()->title() ?>" class="hide-mobile" />
-  			<img src="<?php echo $page->prev()->image('01-thumb.jpg')->url() ?>" alt="<?php echo $page->prev()->title() ?>" class="hide-desktop" />
-  		</div>
-	</div>
-	<?php endif ?>
 </nav>
