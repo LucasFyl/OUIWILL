1615b6160241254849ea5dbbc2e458bb33cc58b3
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-14 14:56:06 +0200

    homeslider snipper logic

---
+++ site/snippets/home-slider.php
@@ -0,0 +1,36 @@
+<section class="landing home">
+	<div class="abs-wrap">
+		<?php snippet('header') ?>
+		<div class="baseline"><p>precisely what you imagined your brand could be, only more so. ™</p></div>
+	</div>
+
+	<div id="mySlider">
+		<div class="slider-content project-infos">
+			<ul>
+				<?php foreach(page('slider')->children() as $slide): ?>
+				<li>
+					<h2><?php echo $slide->title() ?></h2>
+					<p><?php echo $slide->quote() ?></p>
+					<span class="clearfix"></span>
+					<a href="<?php echo $slide->cslink() ?>" class="light-btn white">see case study</a>
+				</li>
+				<?php endforeach ?>
+			</ul>
+		</div>
+		<div class="slider-bg">
+			<?php foreach(page('slider')->children() as $slide): ?>
+			<?php if($image = $slide->images()->sortBy('sort', 'asc')->first()): ?>
+			<img src="<?php echo $image->url() ?>" alt="<?php echo $slide->title() ?>">
+			<?php endif ?>
+			<?php endforeach ?>
+		</div>
+		<div class="slider-nav">
+			<div class="wrap">
+				<div class=""><a href="#" class="nav-link"></a><span class="bar"></span></div>
+				<div class=""><a href="#" class="nav-link"></a><span class="bar"></span></div>
+				<div class=""><a href="#" class="nav-link"></a><span class="bar"></span></div>
+				<div class=""><a href="#" class="nav-link"></a><span class="bar"></span></div>
+			</div>
+		</div>
+	</div>
+</section>
