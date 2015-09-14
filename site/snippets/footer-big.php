4df24f424da9cc536305030629bb7308c330ce31
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-14 12:54:35 +0200

    footer logo link

--- site/snippets/footer-big.php
+++ site/snippets/footer-big.php
@@ -3,7 +3,7 @@
 </footer>
 <footer class="big">
 	<div class="brand">
-		<h1><img src="<?php echo url('assets/images/logo.png') ?>" alt="<?php echo $site->title()->html() ?>" draggable="false"/></h1>
+		<h1><a href="<?php echo $site->url(); ?>"><img src="<?php echo url('assets/images/logo.png') ?>" alt="<?php echo $site->title()->html() ?>" draggable="false"/></a></h1>
 		<h2 class="baseline">precisely what you imagined your brand could be only more so. ™</h2>
 		<div class="social">
 			<ul>
