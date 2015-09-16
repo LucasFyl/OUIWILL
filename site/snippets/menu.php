f026171e55c85e15ce62a8d53cb2cd888d146c82
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-16 14:28:17 +0200

    new nav 90%

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,5 +1,34 @@
 <div id="trigger"><i class="fa fa-bars"></i></div>
+
 <div class="navigation" id="menu">
+  <div class="closer closeMenu"><span class="cross"></span></div>
+  <div class="overlay">
+      <div class="menu">
+      <h1 class="logo"><img src="<?php echo url('assets/images/logo-black.png') ?>" alt="<?php echo $site->title()->html() ?>" draggable="false"/></h1>        
+      <nav>
+        <ul class="cf no-bullet">
+          <!-- <li><a href="<?php echo $site->url() ?>">Home</a></li> -->
+          <li class="hide-desktop"><a href="#" class="closeMenu"></a></li>
+          <?php foreach($pages->visible() as $p): ?>
+          <li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><span><?php echo $p->title()->html() ?></span></a></li>
+          <?php endforeach ?>
+        </ul>
+      </nav>
+      <div class="brand">
+      <div class="social">
+          <ul>
+            <li><a href="https://vimeo.com/user20000853" target="_blank" class="fa fa-vimeo"></a></li>
+            <li><a href="https://www.behance.net/ouiwill" target="_blank" class="fa fa-behance"></a></li>
+            <li><a href="https://www.facebook.com/Oui-Will-521221991292035/" target="_blank" class="fa fa-facebook"></a></li>
+            <li><a href="https://instagram.com/ouiwill/" target="_blank" class="fa fa-instagram"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  
+<!-- <div class="navigation" id="menu">
   <div class="closer closeMenu"><span class="cross"></span></div>
   <div class="brand-wrap">
     <div class="brand">
@@ -18,7 +47,6 @@
   <div class="menu">
     <nav>
       <ul class="cf no-bullet">
-        <!-- <li><a href="<?php echo $site->url() ?>">Home</a></li> -->
         <li class="hide-desktop"><a href="#" class="closeMenu"></a></li>
         <?php foreach($pages->visible() as $p): ?>
         <li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><span><?php echo $p->title()->html() ?></span></a></li>
@@ -27,4 +55,4 @@
     </nav>
   </div>
 </div>
-  
+   -->
