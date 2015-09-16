5a5202c459c9bb523de503569217e47093c7f294
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-16 15:28:37 +0200

    new nav anim

--- assets/js/components/navigation.js
+++ assets/js/components/navigation.js
@@ -6,14 +6,14 @@
 			TweenMax.set('#menu .brand > *', {opacity:0,y:20});
 			TweenMax.set('#menu .menu', {opacity:0,x:'100%'});
 			TweenMax.set('#menu .menu li', {opacity:0,y:20});
-			TweenMax.set('#menu .closer', {x:'-100%'});
+			TweenMax.set('#menu .closer', {x:20});
 			TweenMax.set('#menu .closer span', {opacity:0});
 			
 			var menutimeline = new TimelineMax({paused:true});
 			menutimeline.to('#menu', 0.45, {display:'block',opacity:1,ease:Power2.easeIn})
 						.to('#menu .menu', 0.45, {opacity:1,x:'0%',ease:Power2.easeOut})
-						.to('#menu .closer', 0.25, {x:'-50%',ease:Power2.easeOut}, "-=0.1")
-						.to('#menu .closer span', 0.5, {opacity:1,ease:Power2.easeInOut}, "-=0.25")
+						.to('#menu .closer', 0.25, {x:0,ease:Power2.easeOut}, "-=0.1")
+						.to('#menu .closer span', 0.5, {opacity:1,ease:Power2.easeInOut}, "-=0.35")
 						.staggerTo('#menu .brand > *', 0.5, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.5")
 						.staggerTo('#menu .menu li', 0.5, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.4");
 
@@ -23,9 +23,11 @@
 
 		openMenu: function(menutimeline){
 			TweenMax.set('body', {overflow:'hidden'});
+			menutimeline.timeScale += 3;
 			menutimeline.play();
 		},
 		closeMenu: function(menutimeline){
+			menutimeline.timeScale += 2;
 			menutimeline.reverse();
 		  	setTimeout(function(){
 				TweenMax.set('body', {overflow:'auto'});
