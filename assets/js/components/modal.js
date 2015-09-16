0fea734e8071062df5a3c52f10acb6c8f32e057d
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-16 14:08:06 +0200

    showreel btn

--- assets/js/components/modal.js
+++ assets/js/components/modal.js
@@ -9,26 +9,22 @@
 		},
 		open: function(){
 			TweenMax.set('body', {overflow:'hidden'});
-			TweenMax.to('.modal-fade-screen', 0.5, {visibility:'visible',opacity:1,ease:Power2.easeOut});
-			TweenMax.fromTo('.modal-inner', 0.35, {opacity:0,y:-50}, {y:0,opacity:1,ease:Power2.easeOut,delay:0.5});
+			TweenMax.to('.modal-fade-screen', 0.75, {visibility:'visible',opacity:1,ease:Power2.easeOut});
+			TweenMax.fromTo('.modal-inner', 0.5, {opacity:0,y:-50}, {y:0,opacity:1,ease:Power2.easeOut,delay:0.5});
 			TweenMax.to('.closeModal', 0.25, {visibility:'visible',opacity:1,ease:Power2.easeOut,delay:0.7});
 		},
 		close: function(){
 			TweenMax.to('.closeModal', 0.25, {opacity:0,visibility:'hidden',ease:Power2.easeOut});
-			TweenMax.to('.modal-fade-screen', 0.5, {opacity:0,visibility:'hidden',delay:0.35,ease:Power2.easeIn});
-			TweenMax.fromTo('.modal-inner', 0.35, {y:0,opacity:1}, {opacity:0,y:50,ease:Power2.easeIn});
+			TweenMax.to('.modal-fade-screen', 0.75, {opacity:0,visibility:'hidden',delay:0.35,ease:Power2.easeIn});
+			TweenMax.fromTo('.modal-inner', 0.5, {y:0,opacity:1}, {opacity:0,y:50,ease:Power2.easeIn});
 			TweenMax.set('body', {overflow:'auto'});
 		},
 		bindEvents: function(menutimeline){
-			$('body').on('click', '.play-btn', function(e){
+			$('body').on('click', '.play-btn, .modal-trigger', function(e){
 				e.preventDefault();
 				Modal.open();
 			});
-			$('body').on('click', '.modal-fade-screen', function(e){
-				e.preventDefault();
-				Modal.close();
-			});
-			$('body').on('click', '.closeModal', function(e){
+			$('body').on('click', '.modal-fade-screen, .closeModal', function(e){
 				e.preventDefault();
 				Modal.close();
 			});
