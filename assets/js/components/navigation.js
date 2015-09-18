33638bcf9e538e3460f1f3431ab70253a592e80d
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-18 18:36:13 +0200

    various anim improvements

--- assets/js/components/navigation.js
+++ assets/js/components/navigation.js
@@ -5,16 +5,15 @@
 			TweenMax.set('#menu', {display:'none',opacity:0});
 			TweenMax.set('#menu .brand .social ul > *', {opacity:0,y:20});
 			TweenMax.set('#menu .menu', {opacity:0,x:'100%'});
+			TweenMax.set('#menu h1.logo img', {opacity:0});
 			TweenMax.set('#menu .menu li', {opacity:0,y:20});
-			TweenMax.set('#menu .closer', {x:20});
 			TweenMax.set('#menu .closer span', {opacity:0});
 			
 			var menutimeline = new TimelineMax({paused:true});
 			menutimeline.to('header.black.hidden', 0.25, {top:'-7.8rem',ease:Power2.easeIn})
 						.to('#menu', 0.45, {display:'block',opacity:1,ease:Power2.easeIn})
 						.to('#menu .menu', 0.45, {opacity:1,x:'0%',ease:Power2.easeOut})
-						.to('#menu .closer', 0.25, {x:0,ease:Power2.easeOut}, "-=0.1")
-						.to('#menu .closer span', 0.5, {opacity:1,ease:Power2.easeInOut}, "-=0.35")
+						.to('#menu .closer span, #menu h1.logo img', 0.5, {opacity:1,ease:Power2.easeOut}, '-=0.1')
 						.staggerTo('#menu .brand .social ul > *', 0.5, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.5")
 						.staggerTo('#menu .menu li', 0.5, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.4");
 
@@ -45,7 +44,7 @@
 				}
 			});
 			$(document).on('click', '.closeMenu', function(){
-				TweenMax.to('#menu .closer span', 0.3, {opacity:0,ease:Power2.easeIn});
+				// TweenMax.to('#menu .closer span', 0.3, {opacity:0,ease:Power2.easeIn});
 				Navigation.closeMenu(menutimeline);
 			});
 			$(document).on('click', '.overlay', function(){
