f3bcf8e60e0d5dc4e9a8502c0ba4837ab008a2be
Lucas Fayolle <lfayolle1@gmail.com>
2015-10-30 10:53:59 -0700

    fix navigation closer animation

--- assets/js/components/navigation.js
+++ assets/js/components/navigation.js
@@ -8,16 +8,16 @@
 			TweenMax.set('#menu .menu', {x:'100%'});
 			TweenMax.set('#menu h1.logo img', {opacity:0});
 			TweenMax.set('#menu .menu li', {opacity:0,y:10});
-			// TweenMax.set('#menu .closer span', {opacity:0});
-			
+			TweenMax.set('.closeMenu', {opacity:0});
+
 			var menutimeline = new TimelineMax({paused:true});
-			
 			menutimeline.set('#menu', {display:'block'})
 						.to('#menu .menu', 0.45, {x:'0%',ease:Power1.easeOut})
 			 			.to('#menu .overlay', 0.25, {opacity:1,display:'block'}, "-=0.2")
 						.to('header.black.hidden', 0.25, {top:'-7.8rem',ease:Power2.easeIn}, "-=0.4")
 						.to('#menu h1.logo img', 0.3, {opacity:1,ease:Power2.easeOut}, '-=0.5')
 						.staggerTo('#menu .brand .social ul > *', 0.3, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.5")
+						.to('.closeMenu', 0.25, {opacity:1,ease:Power2.easeOut}, "-=0.25")
 						.staggerTo('#menu .menu li', 0.25, {opacity:1,y:0,ease:Power2.easeOut}, 0.1, "-=0.1");
 
 			this.bindEvents(menutimeline);
@@ -47,7 +47,6 @@
 				}
 			});
 			$(document).on('click', '.closeMenu', function(){
-				// TweenMax.to('#menu .closer span', 0.3, {opacity:0,ease:Power2.easeIn});
 				Navigation.closeMenu(menutimeline);
 			});
 			$(document).on('click', '.overlay', function(e){
